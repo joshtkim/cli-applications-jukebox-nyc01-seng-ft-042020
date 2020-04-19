5943c77853587ea6a5373f2c5b7ed203207c4d2f
Joshua Kim <jykim92@gmail.com>
2020-04-19 01:53:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,3 +21,16 @@ def list(list_songs)
     puts "#{index + 1}. #{song}"
   end
 end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp()
+
+  if (1..9).to_a.index(input.to_i) != nil
+    puts "Playing #{my_songs[input.to_i - 1]}"
+  elsif my_songs.index(input) != nil
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
