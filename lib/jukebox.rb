75a5683abebf7c415abaeda6d62f85c662d3188f
Joshua Kim <jykim92@gmail.com>
2020-04-19 01:46:07 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,9 +1,16 @@
 # Add your code here
-def say_hello(name)
-  "Hi #{name}!"
-end
+#def say_hello(name)
+#  "Hi #{name}!"
+#end
 
-puts "Enter your name:"
-users_name = gets.strip
+#puts "Enter your name:"
+#users_name = gets.strip
 
-puts say_hello(users_name)
+#puts say_hello(users_name)
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
